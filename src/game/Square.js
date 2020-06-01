1cd79bd79cfc78b3d6d4404f7a57a6602dcf60a3
Ionut Vizitiu <ivizitiu@ganintegrity.com>
2020-06-01 17:05:58 +0300

    refactor: Square as a function component

--- src/game/Square.js
+++ src/game/Square.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
-export class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick({ value: 'X' })}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+export function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
