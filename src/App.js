a285f89ae980013ed3e93d4558d8e86257ebc196
Ionut Vizitiu <ivizitiu@ganintegrity.com>
2020-06-01 16:40:59 +0300

    feat: start building tic tac toe game

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function App() {
-  const hi = 'hello you';
-  return <div>{hi}</div>;
-}
-
-export default App;
--- src/game/Game.test.js
+++ src/game/Game.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { Game } from './Game';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Game />);
+  const linkElement = getByText(/Next player/i);
   expect(linkElement).toBeInTheDocument();
 });
